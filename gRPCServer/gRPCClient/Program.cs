c0c95de31045e0ef2c8e9cb01bca0c362e91b908
kangzhouzhou <kk2020jy>
2020-06-02 20:03:25 +0800

    init

---
+++ gRPCServer/gRPCClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using gRPC;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GrpcChannel channel = GrpcChannel.ForAddress("https://127.0.0.1:9999");
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+            HelloRequest request = new HelloRequest();
+            request.Names.Add("1");
+            request.Names.Add("2");
+            request.Names.Add("3");
+
+
+        }
+    }
+}
---
+++ gRPCServer/gRPCServer/Services/GreeterService.cs
@@ -0,0 +1,41 @@
+﻿using gRPC;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gRPCServer.Services
+{
+    public class GreeterService:Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.Run(() =>
+            {
+                HelloReply reply = new HelloReply();
+                foreach (string item in request.Names)
+                {
+                    reply.Msgs.Add("hello=" + item);
+                }
+                return reply;
+            });
+        }
+
+        public override Task SayGoodbye1(GoodbyeRequest request, IServerStreamWriter<GoodbyeReply> responseStream, ServerCallContext context)
+        {
+            return base.SayGoodbye1(request, responseStream, context);
+        }
+
+        public override Task<GoodbyeReply> SayGoodbye2(IAsyncStreamReader<GoodbyeRequest> requestStream, ServerCallContext context)
+        {
+            return base.SayGoodbye2(requestStream, context);
+        }
+
+        public override Task SayGoodbye3(IAsyncStreamReader<GoodbyeRequest> requestStream, IServerStreamWriter<GoodbyeReply> responseStream, ServerCallContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
