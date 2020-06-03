f97a10dabb389fc1443480129701c79ff9726a56
kangzhouzhou <kk2020jy>
2020-06-03 20:03:32 +0800

    2020-06-03

---
+++ gRPCServer/EFConsole/Db/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFConsole.Db
+{
+    /// <summary>
+    /// 博客
+    /// </summary>
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
